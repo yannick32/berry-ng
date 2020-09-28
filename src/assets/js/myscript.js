2e1d9acbf099263d1b12d2e0a18bcba87aeb7f67
Yannick <vanham.yannick@gmail.com>
2020-09-28 14:41:05 +0200

    Add custom script to validate password on the front end

---
+++ src/assets/js/myscript.js
@@ -0,0 +1,9 @@
+function validatePassword(){
+    let password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords don't match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
